67b11dea417436f81956e091de0ca839870d3c0e
Fridolin Pokorny <fridolin@redhat.com>
2018-01-22 11:47:14 +0100

    Add liveness and readiness probes

--- api_v1.py
+++ api_v1.py
@@ -14,13 +14,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(['/result'])
+    return jsonify(['/v1/result', '/readiness', '/liveness'])
 
 
-@app.route('/result', methods=['POST'])
+@app.route('/v1/result', methods=['POST'])
 def post_result():
     if not request.json:
         abort(400)
     # Print to console for now.
     print(json.dumps(request.json))
+    return jsonify(None)
+
+
+@app.route('/readiness')
+def get_readiness():
+    # TODO: extend - check database connection
+    return jsonify(None)
+
+
+@app.route('/liveness')
+def get_liveness():
+    # TODO: extend - check database connection
     return jsonify(None)
