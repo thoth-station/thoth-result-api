f5a3365304f2769b94e71f7ab87f641090dba577
Fridolin Pokorny <fridolin@redhat.com>
2018-02-09 18:01:41 +0100

    Add solver result endpoint

--- wsgi.py
+++ wsgi.py
@@ -17,13 +17,26 @@ def index():
     return jsonify(['/api/v1/result', '/readiness', '/liveness'])
 
 
-@application.route('/api/v1/result', methods=['POST'])
-def post_result():
+@application.route('/api/v1/analsis-result', methods=['POST'])
+def post_analysis_result():
     if not request.json:
         abort(400)
 
     file_name = str(uuid.uuid4()) + '.json'
-    with open(os.path.join(os.environ['THOTH_PERSISTENT_VOLUME_PATH'], file_name), 'w') as output_file:
+    with open(os.path.join(os.environ['THOTH_PERSISTENT_VOLUME_PATH'], 'analysis-' + file_name), 'w') as output_file:
+        json.dump(request.json, output_file, sort_keys=True, indent=2)
+
+    application.logger.info("Result stored to file %r", file_name)
+    return jsonify({'id': file_name})
+
+
+@application.route('/api/v1/solver-result', methods=['POST'])
+def post_solver_result():
+    if not request.json:
+        abort(400)
+
+    file_name = str(uuid.uuid4()) + '.json'
+    with open(os.path.join(os.environ['THOTH_PERSISTENT_VOLUME_PATH'], 'solver-' + file_name), 'w') as output_file:
         json.dump(request.json, output_file, sort_keys=True, indent=2)
 
     application.logger.info("Result stored to file %r", file_name)
