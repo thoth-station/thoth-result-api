2b09849f69515e47d601888f40c444b9cbd4cad8
Fridolin Pokorny <fridolin@redhat.com>
2018-08-03 09:50:28 +0200

    Store adviser results

--- wsgi.py
+++ wsgi.py
@@ -27,6 +27,7 @@ from flask import request
 from thoth.common import init_logging
 from thoth.storages import AnalysisResultsStore
 from thoth.storages import SolverResultsStore
+from thoth.storages import AdvisersResultsStore
 from thoth.storages import __version__ as thoth_storages_version
 
 
@@ -58,7 +59,11 @@ def post_solver_result():  # Ignore PyDocStyleBear
 
 @application.route('/api/v1/adviser-result', methods=['POST'])
 def post_adviser_result():  # Ignore PyDocStyleBear
-    return jsonify({'error': 'Not implemented yet'}), 500, {'ContentType': 'application/json'}
+    adapter = AdvisersResultsStore()
+    adapter.connect()
+    document_id = adapter.store_document(request.json)
+    _LOGGER.info("Adviser result stored with document_id %r", document_id)
+    return jsonify({'document_id': document_id}), 201, {'ContentType': 'application/json'}
 
 
 @application.route('/readiness')
